f1fddc5aa757fbe5be1682d729bc6a1f61419933
veyse <veyse@DESKTOP-2K9PCLA>
2024-03-02 00:44:33 +0300

    Add project files.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,350 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.LblDolarAlis = new System.Windows.Forms.Label();
+            this.LblDolarSatis = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.LblEuroSatis = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.LblEuroAlis = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.TxtKur = new System.Windows.Forms.TextBox();
+            this.TxtMiktar = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.TxtTutar = new System.Windows.Forms.TextBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.TxtKalan = new System.Windows.Forms.TextBox();
+            this.label12 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(41, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(122, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Dolar Alış:";
+            // 
+            // LblDolarAlis
+            // 
+            this.LblDolarAlis.AutoSize = true;
+            this.LblDolarAlis.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.LblDolarAlis.Location = new System.Drawing.Point(200, 27);
+            this.LblDolarAlis.Name = "LblDolarAlis";
+            this.LblDolarAlis.Size = new System.Drawing.Size(33, 36);
+            this.LblDolarAlis.TabIndex = 1;
+            this.LblDolarAlis.Text = "0";
+            // 
+            // LblDolarSatis
+            // 
+            this.LblDolarSatis.AutoSize = true;
+            this.LblDolarSatis.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.LblDolarSatis.Location = new System.Drawing.Point(200, 75);
+            this.LblDolarSatis.Name = "LblDolarSatis";
+            this.LblDolarSatis.Size = new System.Drawing.Size(26, 29);
+            this.LblDolarSatis.TabIndex = 3;
+            this.LblDolarSatis.Text = "0";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(29, 75);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(136, 29);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Dolar Satış:";
+            // 
+            // LblEuroSatis
+            // 
+            this.LblEuroSatis.AutoSize = true;
+            this.LblEuroSatis.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.LblEuroSatis.Location = new System.Drawing.Point(200, 188);
+            this.LblEuroSatis.Name = "LblEuroSatis";
+            this.LblEuroSatis.Size = new System.Drawing.Size(26, 29);
+            this.LblEuroSatis.TabIndex = 7;
+            this.LblEuroSatis.Text = "0";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label6.Location = new System.Drawing.Point(29, 188);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(129, 29);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "Euro Satış:";
+            // 
+            // LblEuroAlis
+            // 
+            this.LblEuroAlis.AutoSize = true;
+            this.LblEuroAlis.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.LblEuroAlis.Location = new System.Drawing.Point(200, 133);
+            this.LblEuroAlis.Name = "LblEuroAlis";
+            this.LblEuroAlis.Size = new System.Drawing.Size(26, 29);
+            this.LblEuroAlis.TabIndex = 5;
+            this.LblEuroAlis.Text = "0";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label8.Location = new System.Drawing.Point(41, 133);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(115, 29);
+            this.label8.TabIndex = 4;
+            this.label8.Text = "Euro Alış:";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label9.Location = new System.Drawing.Point(471, 27);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(56, 29);
+            this.label9.TabIndex = 8;
+            this.label9.Text = "Kur:";
+            // 
+            // TxtKur
+            // 
+            this.TxtKur.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.TxtKur.Location = new System.Drawing.Point(630, 22);
+            this.TxtKur.Name = "TxtKur";
+            this.TxtKur.Size = new System.Drawing.Size(249, 34);
+            this.TxtKur.TabIndex = 9;
+            this.TxtKur.TextChanged += new System.EventHandler(this.TxtKur_TextChanged);
+            // 
+            // TxtMiktar
+            // 
+            this.TxtMiktar.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.TxtMiktar.Location = new System.Drawing.Point(630, 97);
+            this.TxtMiktar.Name = "TxtMiktar";
+            this.TxtMiktar.Size = new System.Drawing.Size(249, 34);
+            this.TxtMiktar.TabIndex = 11;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label10.Location = new System.Drawing.Point(471, 100);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(84, 29);
+            this.label10.TabIndex = 10;
+            this.label10.Text = "Miktar:";
+            // 
+            // TxtTutar
+            // 
+            this.TxtTutar.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.TxtTutar.Location = new System.Drawing.Point(630, 169);
+            this.TxtTutar.Name = "TxtTutar";
+            this.TxtTutar.Size = new System.Drawing.Size(249, 34);
+            this.TxtTutar.TabIndex = 13;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label11.Location = new System.Drawing.Point(471, 172);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(75, 29);
+            this.label11.TabIndex = 12;
+            this.label11.Text = "Tutar:";
+            // 
+            // TxtKalan
+            // 
+            this.TxtKalan.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.TxtKalan.Location = new System.Drawing.Point(630, 240);
+            this.TxtKalan.Name = "TxtKalan";
+            this.TxtKalan.Size = new System.Drawing.Size(249, 34);
+            this.TxtKalan.TabIndex = 15;
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label12.Location = new System.Drawing.Point(471, 243);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(80, 29);
+            this.label12.TabIndex = 14;
+            this.label12.Text = "Kalan:";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(230, 328);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(224, 48);
+            this.button1.TabIndex = 16;
+            this.button1.Text = "İstediğin Miktara Göre";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button2.Location = new System.Drawing.Point(460, 328);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(209, 48);
+            this.button2.TabIndex = 17;
+            this.button2.Text = "Parana Göre";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button3.Location = new System.Drawing.Point(689, 328);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(190, 48);
+            this.button3.TabIndex = 18;
+            this.button3.Text = "Ekranı Temizle";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button4.Location = new System.Drawing.Point(310, 31);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(35, 38);
+            this.button4.TabIndex = 19;
+            this.button4.Text = "...";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button5.Location = new System.Drawing.Point(310, 75);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(35, 38);
+            this.button5.TabIndex = 20;
+            this.button5.Text = "...";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button6.Location = new System.Drawing.Point(310, 133);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(35, 38);
+            this.button6.TabIndex = 21;
+            this.button6.Text = "...";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button7.Location = new System.Drawing.Point(310, 188);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(35, 38);
+            this.button7.TabIndex = 22;
+            this.button7.Text = "...";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Khaki;
+            this.ClientSize = new System.Drawing.Size(933, 567);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.TxtKalan);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.TxtTutar);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.TxtMiktar);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.TxtKur);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.LblEuroSatis);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.LblEuroAlis);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.LblDolarSatis);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.LblDolarAlis);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label LblDolarAlis;
+        private System.Windows.Forms.Label LblDolarSatis;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label LblEuroSatis;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label LblEuroAlis;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox TxtKur;
+        private System.Windows.Forms.TextBox TxtMiktar;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox TxtTutar;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.TextBox TxtKalan;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+    }
+}
+
